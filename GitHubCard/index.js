75687446666d88c240f62817c4a821dc0a776844
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-06 08:00:45 -0500

    Met MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,18 +2,6 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-// axios.get('https://api.github.com/users/nathanmelby')
-//   .then(response => {
-//     console.log(response); 
-//     response.data.map(arrayItem => {
-//     const newPerson = createCard(arrayItem);
-//     cards.appendChild(newPerson);
-//     });
-// });
-  // .catch(error => {
-  //   console.log('The data was not returned', error)
-  // });
- 
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -22,42 +10,6 @@
 
    Skip to Step 3.
 */
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-   // const newGitHubFollowers = data.map((arrayItem) => {
-    //   const newFollowers = createCard(arrayItem.avatar_url, arrayItem.name, arrayItem.login, arrayItem.location, 
-    //     arrayItem.html_url, arrayItem.followers, arrayItem.following, arrayItem.bio);
-        
-    //     return newFollowers;
-    // })
-    
-    // newGitHubFollowers.forEach(component => {
-    //   cardsEntry.appendChild(component);
-    // }) 
-
-
-const followersArray = [
-  // login	"Wais-A"
-  // avatar_url	"https://avatars2.githubusercontent.com/u/12667453?v=4"
-  //  url	"https://api.github.com/users/Wais-A"
-  // html_url	"https://github.com/Wais-A"
-  // followers_url	"https://api.github.com/users/Wais-A/followers"
-  // following_url	"https://api.github.com/users/Wais-A/following{/other_user}"
-
-
-];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -79,10 +31,53 @@ const followersArray = [
 
 */
 
+/* Step 4: Pass the data received from Github into your function, 
+           create a new component and add it to the DOM as a child of .cards
+*/
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
+*/
 
 
+
+const followersArray = [
+
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+
+
+];
+
+
+
+// Entrypoint on website(in the .cards div)
 const cardsEntry = document.querySelector('.cards');
 
+
+//Followers Array API
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then((response) => {
+      console.log(response.data);
+      let newPerson = createCard(response.data);
+      cardsEntry.appendChild(newPerson);
+  });
+});
+
+
+
+//Personal Profile API
 axios.get('https://api.github.com/users/nathanmelby')
 
   .then(response => {
@@ -93,8 +88,11 @@ axios.get('https://api.github.com/users/nathanmelby')
   
 });
 
-// .catch(error => error);
 
+
+
+
+//card creator function
 function createCard(data) {
   const newCard = document.createElement('div');
   const newImage = document.createElement('img');
@@ -109,17 +107,7 @@ function createCard(data) {
   const newBio = document.createElement('p');
 
 
-  //setup structure of elements
-  newCard.appendChild(newImage);
-  newCard.appendChild(newCardInfo);
-  newCardInfo.appendChild(newName);
-  newCardInfo.appendChild(newUserName);
-  newCardInfo.appendChild(newLocation);
-  newCardInfo.appendChild(newProfile);
-  newProfile.appendChild(newProfileLink);
-  newCardInfo.appendChild(newFollowers);
-  newCardInfo.appendChild(newFollowing);
-  newCardInfo.appendChild(newBio);
+ 
 
   //set class names
 
@@ -136,28 +124,30 @@ function createCard(data) {
   newName.textContent = `${data.name}`;
   newUserName.textContent = `${data.login}`;
   newLocation.textContent = `Location: ${data.location}`;
-  newProfile.textContent = 'Profile: ';
-  // newProfileLink.textContent = `${data.html_url}`;
+  newProfile.textContent = `Profile: `;
+  newProfileLink.textContent = `${data.html_url}`;
   newProfileLink.href = `${data.html_url}`;
+  
 
   newFollowers.textContent = `Followers: ${data.followers}`;
   newFollowing.textContent = `Following: ${data.following}`;
   newBio.textContent = `Bio: ${data.bio}`;
 
+ //setup structure of elements
+ 
+  newCard.appendChild(newImage);
+  newCard.appendChild(newCardInfo);
+  newCardInfo.appendChild(newName);
+  newCardInfo.appendChild(newUserName);
+  newCardInfo.appendChild(newLocation);
+  newCardInfo.appendChild(newProfile);
+  newProfile.appendChild(newProfileLink);
+  newCardInfo.appendChild(newFollowers);
+  newCardInfo.appendChild(newFollowing);
+  newCardInfo.appendChild(newBio);
+
   return newCard;
 };
-
-
-// const newGitHubFollowers = data.map((arrayItem) => {
-//   const newFollowers = createCard(arrayItem.avatar_url, arrayItem.name, arrayItem.login, arrayItem.location, 
-//     arrayItem.html_url, arrayItem.followers, arrayItem.following, arrayItem.bio);
-    
-//     return newFollowers;
-// })
-
-// newGitHubFollowers.forEach(component => {
-//   cardsEntry.appendChild(component);
-// })
 
 
 
